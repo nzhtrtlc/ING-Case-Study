10f511699c69dab9ab19dc35f21ed43ede869e94
nzhtrtlc <20neziht@gmail.com>
2025-07-25 16:30:04 +0300

    enabled SPA fallback to prevent 404 on page reload

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {legacyPlugin} from '@web/dev-server-legacy';
+import { legacyPlugin } from '@web/dev-server-legacy';
 
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
@@ -12,8 +12,11 @@ if (!['dev', 'prod'].includes(mode)) {
 }
 
 export default {
-  nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
+  rootDir: '.',
+  appIndex: 'index.html',
+  nodeResolve: { exportConditions: mode === 'dev' ? ['development'] : [] },
   preserveSymlinks: true,
+  historyApiFallback: true,
   plugins: [
     legacyPlugin({
       polyfills: {
